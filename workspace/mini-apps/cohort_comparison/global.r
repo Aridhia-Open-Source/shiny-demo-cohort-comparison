0e691d9be379b987cd63c6c4f6f84bf6c7626737
Yuri Aleksandrov <yurialeksandrov@Aridhia.net>
2016-09-23 12:17:29 +0100

    Added current working state

---
+++ workspace/mini-apps/cohort_comparison/global.r
@@ -0,0 +1,58 @@
+xap.require("ggvis",
+            "dplyr",
+            "shiny",
+            "shinydashboard")
+
+#library(ggvis)
+#library(dplyr)
+#library(shiny)
+#library(shinydashboard)
+
+
+source("modules.r")
+
+#demo <- read.csv("Y:/Data Science Team/Open Data/GOSH/mock-up datasets/Pediatric/pediatrics_demographics.csv")
+#geno <- read.csv("Y:/Data Science Team/Open Data/GOSH/mock-up datasets/Pediatric/pediatrics_genotypes.csv")
+#measures <- read.csv("Y:/Data Science Team/Open Data/GOSH/mock-up datasets/Pediatric/pediatrics_measures.csv")
+#clinics <- read.csv("Y:/Data Science Team/Open Data/GOSH/mock-up datasets/Pediatric/clinic_identifiers.csv")
+
+
+ 
+demo <- xap.read_table("pediatrics_demographics")
+geno <- xap.read_table("pediatrics_genotypes")
+measures <- xap.read_table("pediatrics_measures")
+clinics <- xap.read_table("pediatrics_clinic_identifiers")
+
+cross_var_choices <- colnames(geno[2:11])
+
+
+demo$gender <- factor(demo$gender, levels = c("Female", "Male"))
+measures$treatment <- factor(measures$treatment, levels = c("Budesonide", "Nedocromil", "Placebo"))
+
+geno <- geno[, 1:15]
+
+perc_equal_to <- function(x, a, n = 2) {
+  round(100 * sum(x == a) / length(x), n)
+}
+
+name_map <- colnames(geno[2:11])
+names(name_map) <- colnames(geno[2:11])
+
+
+cross_tab <- function(d, ...) {
+  
+  labels <- data.frame(x = c(-0.2, -0.2, 0.5, 1.5), y = c(0.5, 1.5, 2.2, 2.2), label = c("-", "+", "+", "-"))
+  
+  d %>% ggvis() %>% 
+    layer_rects(x = ~x, y = ~y, x2 = ~x2, y2 = ~y2, fill = ~values, fillOpacity := 0.9, fillOpacity.hover := 1) %>%
+    layer_text(x = ~x + 0.5, y = ~y + 0.5, text := ~values,
+               align := "center", baseline := "middle",
+               fill := "white", fontSize := 20) %>%
+    layer_text(data = labels, x = ~x, y = ~y, text := ~label, fontSize := 20,
+               align := "center", baseline := "middle") %>%
+    hide_axis("x") %>%
+    hide_axis("y")
+  
+}
+
+
---
+++ workspace/mini-apps/cohort_comparison/ui.r
@@ -0,0 +1,158 @@
+source("global.r")
+
+
+
+
+body <- dashboardBody(
+  
+  #tags$head(
+  #  tags$style(
+  #    HTML(
+  #      "
+  #      .inner {
+  #      min-height:130px;
+  #      }
+  #      "
+  #    )
+  #  )
+  #),
+  
+  navbarPage("BC Reporter", id = "navbar",
+    tabPanel("Population Profile", 
+      fluidPage(
+        column(6,
+          box(title = uiOutput("cohort1_summary"), collapsible = TRUE, width = 12,
+            column(6,
+              ggvisOutput("barplot1"),
+              dateRangeInput("date1", "Choose Date of Inclusion: ", start = as.Date("2016-01-01")) 
+              ),
+            column(6,
+              ggvisOutput("density1"),
+              densityFilterUI("density_cohort1")     
+            )
+          ),
+          tabBox(width = 12,
+            tabPanel(title = "Demographics",
+              fluidRow(
+                column(6,
+                  ggvisOutput("sex_cohort1")
+                ),
+                column(6,
+                  ggvisOutput("age_cohort1")
+                )
+              )
+            ),
+            tabPanel(title = "Gene Panels",
+              uiOutput("cohort1_measures"),
+              fluidRow(
+                column(4,
+                  selectInput("cross_var11", "Select First SNP", choices = cross_var_choices, selected = cross_var_choices[1], width = "100%"),
+                  selectInput("cross_var12", "Select Second SNP", choices = cross_var_choices, selected = cross_var_choices[2], width = "100%")
+                ),
+                column(8,
+                  ggvisOutput("cross_tab1")
+                )
+              )
+            ),
+            tabPanel(title = "Vital Signs",
+              fluidRow(
+                column(6,
+                  ggvisOutput("bp_density1")
+                ),
+                column(6,
+                  ggvisOutput("resp_box1")
+                )
+              )
+            ),
+            tabPanel(title = "Treatments",
+              fluidRow(
+                column(6,
+                  ggvisOutput("treatment_bars1")
+                ),
+                column(6,
+                  ggvisOutput("treatment_points1")
+                )
+              )
+            )
+          ),
+          column(4, offset = 4,
+            actionButton("report", h4("View Report"), width = "100%", icon = icon("file-pdf-o", "fa-3x"))
+          )
+        ),
+        column(6,
+          box(title = uiOutput("cohort2_summary"), collapsible = TRUE, width = 12,
+            column(6,
+              ggvisOutput("barplot2"),
+              dateRangeInput("date2", "Choose Date of Inclusion: ", start = as.Date("2016-01-01"))
+            ),
+            column(6,
+              ggvisOutput("density2"),
+              densityFilterUI("density_cohort2")
+            )
+          ),
+          tabBox(width = 12,
+            tabPanel(title = "Demographics",
+              fluidRow(
+                column(6,
+                  ggvisOutput("sex_cohort2")
+                ),
+                column(6,
+                  ggvisOutput("age_cohort2")
+                )
+              )
+            ),
+            tabPanel(title = "Gene Panels",
+              uiOutput("cohort2_measures"),
+              fluidRow(
+                column(4,
+                  selectInput("cross_var21", "Select First SNP", choices = cross_var_choices, selected = cross_var_choices[1], width = "100%"),
+                  selectInput("cross_var22", "Select Second SNP", choices = cross_var_choices, selected = cross_var_choices[2], width = "100%")
+                ),
+                column(8,
+                  ggvisOutput("cross_tab2")
+                )
+              )
+            ),
+            tabPanel(title = "Vital Signs",
+              fluidRow(
+                column(6,
+                  ggvisOutput("bp_density2")
+                ),
+                column(6,
+                  ggvisOutput("resp_box2")
+                )
+              )
+            ),
+            tabPanel(title = "Treatments",
+              fluidRow(
+                column(6,
+                  ggvisOutput("treatment_bars2")
+                ),
+                column(6,
+                  ggvisOutput("treatment_points2")
+                )
+              )
+            )
+          )
+        )
+      )
+    ),
+      tabPanel("Participant List",
+      participantListUI("pl")
+    ),
+    tabPanel("Report",
+      uiOutput("pdf")
+    )
+  )
+  
+)
+
+
+
+
+header <- dashboardHeader(disable = TRUE)
+sidebar <- dashboardSidebar(disable = TRUE)
+
+
+ui <- dashboardPage(header, sidebar, body)
+
